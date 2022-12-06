cc6bfd234e895ea27be9761bb8c20355f1b523d1
Igor Soldera <ivsoldera@gmail.com>
2022-12-06 13:21:35 -0300

    Ajuste no tamanho permitido por requisição

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import errorMiddleware from './middlewares/errorMiddleware';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(routers);
 
 app.use(errorMiddleware);
